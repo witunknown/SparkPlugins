a5908a20c11525c3da94a015bbbcc63b2700bf55
sunwenchi <562344464@qq.com>
2020-07-16 00:29:51 +0800

    feat:自定义分区器；

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,8 @@
-package base;
-
 /**
  * Created byX on 2020-07-13 23:58
  * Desc:
  */
-public class application {
+public class Application {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
